1b262e36ac34250fe91f4f3e885fe8e222605887
Charlie Hengemihle <charliehengemihle@gmail.com>
2023-01-18 12:06:56 -0700

    all tests passing

--- reduce.js
+++ reduce.js
@@ -2,9 +2,14 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  return brands.reduce((shoeList, brand) => shoeList.concat(brand.shoes), []);
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) => {};
+export const getAverageAge = (dogs) => {
+  const ageTotal = dogs.reduce((acc, dog) => (acc += dog.age), 0) / dogs.length;
+  return ageTotal;
+};
